3e2af382fd21f7d1021a0265f9ec54599b298bf9
rinshad101 <rinshadkk1111@gmail.com>
2025-10-30 12:53:55 +0530

    first commit

---
+++ Message-Service/src/test/java/com/StreamLite/Message_Service/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StreamLite.Message_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
